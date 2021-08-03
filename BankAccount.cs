cd834fbefe104cb328745ad47dd9693dba83e443
nesherson <nesad.omeragich27@gmail.com>
2021-08-03 23:18:40 +0200

    Implement deposit and withdrawal functionality

--- BankAccount.cs
+++ BankAccount.cs
@@ -9,15 +9,40 @@ namespace csharp_workshop {
         private List<Transaction> allTransactions = new List<Transaction>();
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance { get {
+            decimal balance = 0;
+            foreach(var item in allTransactions) {
+                balance += item.Amount;
+            }
+            return balance;
+        } }
          public BankAccount(string name, decimal initialBalance) {
             Owner = name;
-            Balance = initialBalance;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
             Number = accountNumberSeed.ToString();
             accountNumberSeed++;
         }
 
-        public void MakeDeposit(decimal amount, DateTime date, string note) {}
-        public void MakeWithdrawal(decimal amount, DateTime date, string note) {}
+        public void MakeDeposit(decimal amount, DateTime date, string note) {
+            if (amount <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive value");
+            }
+
+            var deposit = new Transaction(amount, date, note);
+
+            allTransactions.Add(deposit);
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note) {
+            if (amount <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive value");
+            }
+
+            if (Balance - amount < 0) {
+                throw new InvalidOperationException("Not insufficient funds for this withdrawal");
+            }
+
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,11 @@ namespace csharp_workshop
             var account = new BankAccount("Amer", 1000);
 
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with balance: {account.Balance}$");
+            account.MakeDeposit(200, DateTime.Now, "Another 200$");
+            account.MakeWithdrawal(700, DateTime.Now, "Bought Xiaomi Killer");
+            Console.WriteLine($"Account now has: {account.Balance}$");
+            
+            
         }
     }
 }
