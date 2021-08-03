1e86764188f99d77f34705e65b5d5768867ac204
nesherson <nesad.omeragich27@gmail.com>
2021-08-03 19:55:06 +0200

    Add transaction class

--- BankAccount.cs
+++ BankAccount.cs
@@ -1,14 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 namespace csharp_workshop {
 
     public class BankAccount {
+
+        private static int accountNumberSeed = 100;
+        private List<Transaction> allTransactions = new List<Transaction>();
         public string Number { get; }
         public string Owner { get; set; }
         public decimal Balance { get; }
          public BankAccount(string name, decimal initialBalance) {
             Owner = name;
             Balance = initialBalance;
+            Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note) {}
